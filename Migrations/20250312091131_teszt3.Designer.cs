39d6a10eb2e139d075087b042e3d61d3d9789c9b
slowee0 <tamasrakoczy7@gmail.com>
2025-03-12 10:14:10 +0100

    pastmodels json javítása

--- Migrations/20250312091131_teszt3.Designer.cs
+++ Migrations/20250312091131_teszt3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VroomWiki.Data;
 
@@ -10,9 +11,11 @@ using VroomWiki.Data;
 namespace VroomWiki.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250312091131_teszt3")]
+    partial class teszt3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
