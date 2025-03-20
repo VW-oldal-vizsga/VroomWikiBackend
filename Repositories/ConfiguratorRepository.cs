9f32fef9c61591ffdc4a28281982336dbcc47896
slowee0 <tamasrakoczy7@gmail.com>
2025-03-20 10:39:14 +0100

    apró javítás

--- Repositories/ConfiguratorRepository.cs
+++ Repositories/ConfiguratorRepository.cs
@@ -28,12 +28,6 @@ namespace VroomWiki.Repositories
             if (configuration == null)
                 return null!;
             return Convert.FromBase64String(configuration.ImageBase64);
-        }public byte[] GetImageColor(int id)
-        {
-            var configColor = context.Set<ConfigColor>().SingleOrDefault(p => p.Id == id);
-            if (configColor == null)
-                return null!;
-            return Convert.FromBase64String(configColor.ImageBase64);
         }
 
         public IEnumerable<ConfigColor> GetAllColor()
@@ -44,6 +38,14 @@ namespace VroomWiki.Repositories
         public IEnumerable<ConfigColor> GetOneColor(int id)
         {
             return context.Set<ConfigColor>().Where(p => p.Id == id);
+        }
+
+        public byte[] GetImageColor(int id)
+        {
+            var configColor = context.Set<ConfigColor>().SingleOrDefault(p => p.Id == id);
+            if (configColor == null)
+                return null!;
+            return Convert.FromBase64String(configColor.ImageBase64);
         }
 
         public IEnumerable<ConfigEngine> GetAllEngines()
